42541fc91b3183000d1d16b209f99b2a828127dc
Vibhu Agarwal <vibhu4agarwal@gmail.com>
2019-01-24 01:59:06 +0530

    Initial Setup Code

---
+++ helpScript.py
@@ -0,0 +1,36 @@
+avengers = {
+	0: 'Captain America',
+	1: 'Black Widow',
+	2: 'Iron Man',
+	3: 'Thor',
+	4: 'Hulk'
+}
+
+captain_america_hints = [
+"On your left.",
+"I know how it feels to suffer frost bite."]
+
+black_widow_hints = [
+"Bruce is only in my control.",
+"I'm gonna kill Thanos with a stick."]
+
+iron_man_hints = [
+"Please pass me some pepper.",
+"Genius. Billionaire. Playboy. Philanthropist."]
+
+thor_hints = [
+"Have you seen my, uh, Natalie Portman?",
+"Can someone give me a horse to travel?"]
+
+hulk_hints = [
+"Puny. God.",
+"I love Natasha."
+]
+
+avengerHint = {
+	0: captain_america_hints,
+	1: black_widow_hints,
+	2: iron_man_hints,
+	3: thor_hints,
+	4: hulk_hints
+}
